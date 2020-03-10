194c18d128bc320627a463bc3023f138b9f999bc
interpreted-network-8638 <muratibuki@gmail.com>
2020-03-10 16:39:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -18,3 +18,19 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
 }
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
